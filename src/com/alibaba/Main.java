b09d6ed544d0ff15ee66faa935d2174ddc8d1794
wenfei.swf <wenfei.swf@alibaba-inc.com>
2020-08-03 19:37:59 +0800

    feat: pull file

---
+++ src/com/alibaba/Main.java
@@ -0,0 +1,12 @@
+package com.alibaba;// 本题为考试多行输入输出规范示例，无需提交，不计分。
+
+public class Main {
+    public static void main(String[] args) {
+        Boolean t1 = new Boolean(true);
+        if (t1) {
+            System.out.println("sldj");
+        }
+    }
+
+
+}
